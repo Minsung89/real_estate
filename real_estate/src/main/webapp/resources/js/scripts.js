d0c10845cb7d25b19d1b22107b8135205d99edf6
Minsung89 <minsung0217@gmail.com>
2021-08-04 13:26:58 +0900

    메일인증 구현

--- real_estate/src/main/webapp/resources/js/scripts.js
+++ real_estate/src/main/webapp/resources/js/scripts.js
@@ -69,4 +69,20 @@ window.addEventListener('DOMContentLoaded', event => {
     	console.log($('input[name=description]').val());
     	console.log($('input[name=nation]').val());
 	});
+    
+   $('#send_email').click(function(){   //submit 버튼을 클릭하였을 때
+		var email = $('#m_email').text(); 
+	    let sendData = {'email' : email};//폼의 이름 값을 변수 안에 담아줌
+		console.log(sendData);
+		$.ajax({
+			type:'post',   //post 방식으로 전송
+			url:'/my_profile/auth-email',   //데이터를 주고받을 파일 주소
+			data:sendData,   //위의 변수에 담긴 데이터를 전송해준다.
+			dataType:'text',   //html 파일 형식으로 값을 담아온다.
+			success : function(data){   //파일 주고받기가 성공했을 경우. data 변수 안에 값을 담아온다.
+				console.log(data);  //현재 화면 위 id="message" 영역 안에 data안에 담긴 html 코드를 넣어준다. 
+			}
+		});
+	});
 });
+
